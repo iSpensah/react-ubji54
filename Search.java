da76ed6203bd97ef685850bcc42cacc2adb0f035
Spencer Robles <105832122+iSpensah@users.noreply.github.com>
2022-06-06 11:15:05 +0800

    Add files via upload

--- Search.java
+++ Search.java
@@ -1,11 +1,21 @@
-package Main;
+
+import java.sql.SQLException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-import jdbc.*;
+
+class NegativeNotAllowed extends Exception {
+	void print() {
+		System.out.println("Negative Not Allowed!");
+	}
+}
 
 public class Search {
 	
-	public static void main (String[] args) {
-		
+	static int accpin,accid,amnt;
+	
+	public static void main (String[] args) throws SQLException, NegativeNotAllowed {
+	
+		try {
 		Scanner in = new Scanner(System.in);
 		dbFunctions db = new dbFunctions();
 		String choice = null;
@@ -13,6 +23,7 @@ public class Search {
 		while(choice !="e") {
 		System.out.println("Enter type of Transaction:/ s for search/ d for DEPO / w for WITHDRAW");
 		choice = in.next();
+		
 		switch(choice) {
 
 		case "s":
@@ -25,21 +36,45 @@ public class Search {
 		case "d":
 		//perform deposit transaction
 		System.out.println("Enter Accountid , accpin, Deposit Amount");
-		int accid = in.nextInt();
-		int accpin = in.nextInt();
-		int amnt = in.nextInt();
-		db.Deposit(accid, accpin, amnt);
+		 accid = in.nextInt();  //example of static variable
+		 accpin = in.nextInt(); //example of static variable
+		if(db.validate(accid, accpin)==true) {
+			System.out.println("Enter Deposit Amount");
+			amnt = in.nextInt();
+			if(amnt < 0) { throw new NegativeNotAllowed(); }
+			else db.Deposit(accid, accpin, amnt);
+		}
+		else {
+			System.out.println("Account pin Does not Match");
+		}
 		break;
 		
 		case "w":
 		//perform withdraw transaction
 		System.out.println("Enter Accountid , accpin, Withdraw Amount");
-		int accid2 = in.nextInt();
-		int accpin2 = in.nextInt();
-		int wdamnt = in.nextInt();
-		db.Withdraw(accid2, accpin2, wdamnt);
+		accid = in.nextInt();  //example of static variable
+		accpin = in.nextInt();  //example of static variable
+		if(db.validate(accid, accpin)==true) {
+			System.out.println("Enter Withdraw Amount");
+		amnt = in.nextInt();
+		if(amnt < 0) { throw new NegativeNotAllowed();}
+		else db.Withdraw(accid, accpin, amnt);
+		}else {
+			System.out.println("Account pin Does not Match");
+		}
 		break;
 		}
 	  }
+		} catch(InputMismatchException e) {
+			main(args);
+		} catch(ArithmeticException e) {
+			main(args);
+		}catch (NegativeNotAllowed e) {
+			e.print();
+			main(args);
+		}
+		catch (Exception e) {
+			main(args);
+		}
 	}
 }
